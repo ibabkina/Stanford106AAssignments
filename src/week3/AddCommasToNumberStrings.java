7cb695aaaf2bc18837bd5eca10ecf3b6d5c83b23
ibabkina <i.babkina700@gmail.com>
2021-04-25 17:19:48 -0400

    week3 assignments added

--- src/week3/AddCommasToNumberStrings.java
+++ src/week3/AddCommasToNumberStrings.java
@@ -1,11 +1,38 @@
 package week3;
 
+/** 
+ * This program takes a string of decimal digits representing a number and returns the string
+ * formed by inserting commas at every third position, starting on the right. 
+ *  
+ * @author Irina Babkina
+ * 
+ */
+
 import acm.program.ConsoleProgram;
 
 public class AddCommasToNumberStrings extends ConsoleProgram {
 	
 	public void run() {
-
+        while (true) {
+        	String digits = readLine("Enter a numeric string: "); 
+        	if (digits.length() == 0) break; 
+        	println(addCommasToNumericString(digits));
+        }
 	}
 	
+	private String addCommasToNumericString(String digits) {
+		if(digits.charAt(0) == '0' && digits.length() > 1) {
+			return "Please do not include 0's in front of your numeric string if your string is greater than one character!";
+		}
+		
+		String newStr = " ";
+		newStr += digits.charAt(0);
+		for(int i = 1; i < digits.length(); i++) {
+			if((digits.length() - i) % 3 == 00 && (digits.length() - i) > 0) {
+				newStr += ","; 
+			}
+			newStr += digits.charAt(i);
+		}	
+		return newStr;
+	}	
 }
--- src/week3/DeleteCharactersFromString.java
+++ src/week3/DeleteCharactersFromString.java
@@ -1,11 +1,32 @@
 package week3;
 
+/** 
+ * This program removes all occurrences of the specified character from the specified string.
+ * 
+ * @author Irina Babkina
+ * 
+ */
+
 import acm.program.ConsoleProgram;
 
 public class DeleteCharactersFromString extends ConsoleProgram {
 	
 	public void run() {
+		
+		println(removeAllOccurrences("This is a test", 't'));  	// returns "This is a es"
+		println(removeAllOccurrences("Summer is here!", 'e')); 	// returns "Summr is hr"
+		println(removeAllOccurrences("---0---", '-'));			// returns "0"
 
 	}
 	
+	public String removeAllOccurrences(String str, char ch) {
+		String newStr = " ";
+		for(int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == ch) {
+				continue;
+			}
+			newStr += str.charAt(i);
+		}
+		return newStr;
+	}	
 }
